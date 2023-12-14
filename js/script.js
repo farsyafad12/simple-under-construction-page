c13f852134a0fb9b31e0d39b64bec06c1a7e1294
FarSyaFad <farisfadlansyakilah12@gmail.com>
2023-12-14 22:25:41 +0700

    V.1.0

---
+++ js/script.js
@@ -0,0 +1,26 @@
+endDate = new Date("January 31, 2024 23:59:59").getTime();
+const x = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = endDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+    document.getElementById("hours").innerHTML = hours
+        .toString()
+        .padStart(2, "0");
+    document.getElementById("minutes").innerHTML = minutes
+        .toString()
+        .padStart(2, "0");
+    document.getElementById("seconds").innerHTML = seconds
+        .toString()
+        .padStart(2, "0");
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "Waktu sudah habis!";
+    }
+}, 1000);
